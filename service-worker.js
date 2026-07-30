5fe1d57be583c97a3eb508d44e7d16277f192c5a
suprafast1979 <suprafast1979@gmail.com>
2026-07-30 07:59:49 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
-
-const CACHE = 'sprinkler-planner-v1';
+const CACHE = 'sprinkler-planner-v2';
 const APP_FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
@@ -15,11 +14,14 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match('./index.html')))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
   );
 });
